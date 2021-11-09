16acd24b57757be5312d84f7ba8b6c4171490a98
Mohamed.Hasanen <m.elsayed.m.1995@gmail.com>
2021-11-09 17:09:40 +0200

    Add MMPK

--- regionfeatureextraction/src/main/java/com/alpha/regionfeatureextraction/MainActivity.kt
+++ regionfeatureextraction/src/main/java/com/alpha/regionfeatureextraction/MainActivity.kt
@@ -1,11 +1,78 @@
 package com.alpha.regionfeatureextraction
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.alpha.regionfeatureextraction.databinding.ActivityMainBinding
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.esri.arcgisruntime.loadable.LoadStatus
+import com.esri.arcgisruntime.mapping.MobileMapPackage
+import com.esri.arcgisruntime.mapping.view.MapView
+import java.io.File
+import java.io.FileNotFoundException
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy{ ActivityMainBinding.inflate(layoutInflater) }
+    private val mapView: MapView by lazy { binding.mapView }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupMap()
+
+        val mobileMapPackage = MobileMapPackage(getFileFromAssets(MapsConfigurations.OfflineMapbook)?.absolutePath)
+
+        mobileMapPackage.loadAsync()
+        mobileMapPackage.addDoneLoadingListener {
+            if(mobileMapPackage.loadStatus == LoadStatus.LOADED && mobileMapPackage.maps.isNotEmpty()){
+                mapView.map = mobileMapPackage.maps.first()
+                val locator = mobileMapPackage.locatorTask
+
+                if(locator == null)
+                    Toast.makeText(this, "No Locator", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Error occurred while loading map!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+
+    fun getFileFromAssets(fileName: String): File? {
+        return try {
+            File(this.cacheDir, fileName)
+                .also {
+                    it.outputStream()
+                        .use { cache ->
+                            this.assets.open(fileName)
+                                .use { inputStream -> inputStream.copyTo(cache) }
+                        }
+                }
+        } catch (exception: FileNotFoundException){
+            null
+        }
+
+    }
+
+
+
+    private fun setupMap() {
+        ArcGISRuntimeEnvironment.setApiKey(MapsConfigurations.API_KEY)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.resume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.dispose()
     }
 }
---
+++ regionfeatureextraction/src/main/java/com/alpha/regionfeatureextraction/MapsConfigurations.kt
@@ -0,0 +1,11 @@
+package com.alpha.regionfeatureextraction
+
+object MapsConfigurations {
+    const val API_KEY = "AAPKaaafd6d225b742a696960979e109d5d0nPd1qCxCFA22JXT6An8W4WLd31lNTvyX1ScK0RC9GeqyURCiAYUPjqk0_c3kDVdJ"
+    const val GasDeviceAnno = "GasDeviceAnno.mmpk"
+    const val OfflineMapbook = "OfflineMapbook.mmpk"
+    const val PSCC_MMPK_With_Network = "PSCC_MMPK_With_Network.mmpk"
+    const val SanFrancisco1 = "SanFrancisco1.mmpk"
+    const val SanFrancisco2 = "SanFrancisco2.mmpk"
+    const val Yellowstone = "Yellowstone.mmpk"
+}
