f80cbc2e0ee54f8183f52298a7ba0f89b5d41d9a
Mohamed.Hasanen <m.elsayed.m.1995@gmail.com>
2021-11-10 10:55:49 +0200

    Show Callout with some features

--- regionfeatureextraction/src/main/java/com/alpha/regionfeatureextraction/MainActivity.kt
+++ regionfeatureextraction/src/main/java/com/alpha/regionfeatureextraction/MainActivity.kt
@@ -1,31 +1,43 @@
 package com.alpha.regionfeatureextraction
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.graphics.Point
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alpha.regionfeatureextraction.databinding.ActivityMainBinding
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
 import com.esri.arcgisruntime.concurrent.ListenableFuture
-import com.esri.arcgisruntime.loadable.LoadStatus
 import com.esri.arcgisruntime.mapping.ArcGISMap
-import com.esri.arcgisruntime.mapping.MobileMapPackage
+import com.esri.arcgisruntime.mapping.view.Callout
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener
 import com.esri.arcgisruntime.mapping.view.IdentifyLayerResult
 import com.esri.arcgisruntime.mapping.view.MapView
 import com.esri.arcgisruntime.portal.Portal
 import com.esri.arcgisruntime.portal.PortalItem
-import java.io.File
-import java.io.FileNotFoundException
-import java.lang.Exception
 import kotlin.math.roundToInt
+
+/**
+ * 1- Extracting and display features from web map service.
+ *
+ *
+ * References:-
+ * - [Display a web map](https://developers.arcgis.com/android/maps-2d/tutorials/display-a-web-map/)
+ * - https://developers.arcgis.com/android/kotlin/sample-code/feature-layer-selection/
+ * - https://developers.arcgis.com/android/java/sample-code/feature-layer-show-attributes/
+ */
 
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy{ ActivityMainBinding.inflate(layoutInflater) }
     private val mapView: MapView by lazy { binding.mapView }
+
+    private val callout: Callout by lazy { mapView.callout }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,9 +56,10 @@ class MainActivity : AppCompatActivity() {
                 val identifyLayerResultFuture: ListenableFuture<List<IdentifyLayerResult>> = mapView.identifyLayersAsync(screenPoint, 0.0, false)
                 identifyLayerResultFuture.addDoneListener {
                     try {
+                        if(callout.isShowing) callout.dismiss()
                         val listIdentifyLayerResult: List<IdentifyLayerResult> = identifyLayerResultFuture.get()
                         if(listIdentifyLayerResult.isNotEmpty()){
-                            Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_LONG).show()
+                            displayFeatures(listIdentifyLayerResult.first())
                         } else {
                             Toast.makeText(this@MainActivity, "Nothing", Toast.LENGTH_SHORT).show()
                         }
@@ -54,9 +67,40 @@ class MainActivity : AppCompatActivity() {
                         ex.printStackTrace()
                     }
                 }
-
                 return true
             }
+
+            override fun onDoubleTap(e: MotionEvent?): Boolean {
+                if (callout.isShowing) callout.dismiss()
+                return super.onDoubleTap(e)
+            }
+        }
+    }
+
+    private fun displayFeatures(identifyLayerResult: IdentifyLayerResult) {
+        val attributes = identifyLayerResult.elements[0].attributes
+        val calloutContent = getCalloutContent()
+
+        for(attribute in attributes){
+            calloutContent.append("Key: ${attribute.key}, Value: ${attribute.value ?: "N/A"}")
+        }
+
+        val envelope = identifyLayerResult.elements[0].geometry.extent
+        mapView.setViewpointGeometryAsync(envelope, 200.0)
+        callout.location = envelope.center
+        callout.content = calloutContent
+        callout.show()
+
+    }
+
+    private fun getCalloutContent(): TextView{
+        return TextView(this).apply {
+            setTextColor(Color.BLACK)
+            isSingleLine = false
+            isVerticalScrollBarEnabled = true
+            scrollBarStyle = View.SCROLLBARS_INSIDE_INSET
+            movementMethod = ScrollingMovementMethod()
+            setLines(5)
         }
     }
 
