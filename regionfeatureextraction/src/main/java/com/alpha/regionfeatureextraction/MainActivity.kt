43cbd30b5702f418e1e8c259f7ee7cdd82837331
Mohamed.Hasanen <m.elsayed.m.1995@gmail.com>
2021-11-10 09:17:33 +0200

    Switch to mapView

--- regionfeatureextraction/src/main/java/com/alpha/regionfeatureextraction/MainActivity.kt
+++ regionfeatureextraction/src/main/java/com/alpha/regionfeatureextraction/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.alpha.regionfeatureextraction
 
+import android.annotation.SuppressLint
+import android.graphics.Point
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alpha.regionfeatureextraction.databinding.ActivityMainBinding
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.esri.arcgisruntime.concurrent.ListenableFuture
 import com.esri.arcgisruntime.loadable.LoadStatus
+import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.MobileMapPackage
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener
+import com.esri.arcgisruntime.mapping.view.IdentifyLayerResult
 import com.esri.arcgisruntime.mapping.view.MapView
+import com.esri.arcgisruntime.portal.Portal
+import com.esri.arcgisruntime.portal.PortalItem
 import java.io.File
 import java.io.FileNotFoundException
+import java.lang.Exception
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,44 +32,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setupMap()
 
-        val mobileMapPackage = MobileMapPackage(getFileFromAssets(MapsConfigurations.OfflineMapbook)?.absolutePath)
+        handleTouchListener()
+    }
 
-        mobileMapPackage.loadAsync()
-        mobileMapPackage.addDoneLoadingListener {
-            if(mobileMapPackage.loadStatus == LoadStatus.LOADED && mobileMapPackage.maps.isNotEmpty()){
-                mapView.map = mobileMapPackage.maps.first()
-                val locator = mobileMapPackage.locatorTask
+    @SuppressLint("ClickableViewAccessibility")
+    private fun handleTouchListener() {
+        mapView.onTouchListener = object : DefaultMapViewOnTouchListener(this, mapView){
+            override fun onSingleTapConfirmed(motionEvent: MotionEvent): Boolean {
+                val screenPoint = Point(motionEvent.x.roundToInt(), motionEvent.y.roundToInt())
 
-                if(locator == null)
-                    Toast.makeText(this, "No Locator", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(this, "Error occurred while loading map!", Toast.LENGTH_LONG).show()
+                val identifyLayerResultFuture: ListenableFuture<List<IdentifyLayerResult>> = mapView.identifyLayersAsync(screenPoint, 0.0, false)
+                identifyLayerResultFuture.addDoneListener {
+                    try {
+                        val listIdentifyLayerResult: List<IdentifyLayerResult> = identifyLayerResultFuture.get()
+                        if(listIdentifyLayerResult.isNotEmpty()){
+                            Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_LONG).show()
+                        } else {
+                            Toast.makeText(this@MainActivity, "Nothing", Toast.LENGTH_SHORT).show()
+                        }
+                    }catch (ex: Exception){
+                        ex.printStackTrace()
+                    }
+                }
+
+                return true
             }
         }
     }
 
 
-
-    fun getFileFromAssets(fileName: String): File? {
-        return try {
-            File(this.cacheDir, fileName)
-                .also {
-                    it.outputStream()
-                        .use { cache ->
-                            this.assets.open(fileName)
-                                .use { inputStream -> inputStream.copyTo(cache) }
-                        }
-                }
-        } catch (exception: FileNotFoundException){
-            null
-        }
-
-    }
-
-
-
     private fun setupMap() {
         ArcGISRuntimeEnvironment.setApiKey(MapsConfigurations.API_KEY)
+        val portal = Portal("https://www.arcgis.com", false)
+        val portalItem = PortalItem(portal, "41281c51f9de45edaf1c8ed44bb10e30")
+        mapView.map = ArcGISMap(portalItem)
     }
 
     override fun onPause() {
